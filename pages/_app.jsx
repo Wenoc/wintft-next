79c198a9b0bda95a6e37debb8a1897ae6bf4902f
Wenoc <eocrin.game@gmail.com>
2022-12-07 09:59:00 +0000

    font from bunny.net

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,25 @@
 // eslint-disable-next-line import/no-unresolved
+import Head from "next/head";
+// eslint-disable-next-line import/no-unresolved
 import { Analytics } from "@vercel/analytics/react";
 import Layout from "../components/general/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Analytics />
-    </Layout>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+          href="https://fonts.bunny.net/css?family=noto-sans:400,500,600,700,800"
+          rel="stylesheet"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </>
   );
 }
 
